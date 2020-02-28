6283f221cd237d47030abac0a0906c2676c991b9
q190776912 <190776912@qq.com>
2020-02-28 18:19:54 +0800

    初步完成倒计时模块

--- pages/phone-login/phone-login.js
+++ pages/phone-login/phone-login.js
@@ -1,12 +1,52 @@
-// pages/phone-login/phone-login.js
+const COUNT_DOWN_NUM = 10;
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    isSendCode: false,
+    phoneNumber: '15625120668',
+    countNum: COUNT_DOWN_NUM
   },
 
-  
+  getPhoneNumber(event) {
+    this.setData({
+      phoneNumber: event.detail.value
+    })
+  },
+
+  getVcode() {
+    if (this.data.isSendCode) {
+      return;
+    }
+    const reg = /^1[3456789][0-9]{9}$/
+    if (this.data.phoneNumber && reg.test(this.data.phoneNumber)) {
+      this.setData({
+        isSendCode: true
+      })
+      this.timeDown();
+    } else {
+      wx.showToast({
+        title: '请输入正确格式的手机号码',
+        icon: 'none'
+      })
+    }
+  },
+
+  timeDown() {
+    const timer = setInterval(() => {
+      let countNum = this.data.countNum;
+      if (--countNum) {
+        this.setData({ countNum })
+      } else {
+        clearInterval(timer);
+        this.setData({
+          countNum: COUNT_DOWN_NUM,
+          isSendCode: false
+        })
+      }
+    }, 1000)
+  }
 })
