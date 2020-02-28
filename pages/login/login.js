21f053d009506bf466afd2e51db78b38b1894864
q190776912 <190776912@qq.com>
2020-02-28 09:15:40 +0800

    微信授权

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,13 +5,47 @@ Page({
   },
 
   onGotUserInfo(e) {
-    const detail = e.detail;
-    if (!detail.userInfo) {
+    const userInfo = e.detail.userInfo
+    if (!userInfo) {
       wx.showToast({
         title: '用户授权失败',
         icon: 'none'
       })
       return
     }
+    wx.login({
+      success(res) {
+        if (res.code) {
+          wx.showLoading({
+            title: '加载中',
+          })
+          wx.request({
+            url: 'http://localhost:3000/api/user/wxlogin',
+            data: {
+              code: res.code,
+              nickname: userInfo.nickName,
+              avatar: userInfo.avatarUrl
+            },
+            method: 'POST',
+            success(res) {
+              wx.showToast({
+                title: res.data.message,
+                success() {
+                  console.log(res)
+                  wx.navigateTo({
+                    url: '/pages/home/home',
+                  })
+                }
+              })
+            },
+            complete() {
+              wx.hideLoading()
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
   }
 })
